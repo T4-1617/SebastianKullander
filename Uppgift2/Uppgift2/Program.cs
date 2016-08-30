10cfbc57a235ee0833ba3e91ed524a5bd060c801
sebb <kvakod@gmail.com>
2016-08-30 12:27:44 +0200

    Initial commit

---
+++ Uppgift2/Uppgift2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej vad heter du?");
+            Console.Write("Skriv in ditt förnamn:");
+            string namn = Console.ReadLine();
+
+            Console.Write("skriv in ditt efternamn:");
+            string efternamn = Console.ReadLine();
+
+            Console.WriteLine("hej " + namn + " " +efternamn);
+            Console.ReadLine();
+
+
+        }
+    }
+}
