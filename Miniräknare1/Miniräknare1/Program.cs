a77854dc578b768c737ef97408dbbfd301d8473c
sebb <kvakod@gmail.com>
2016-08-31 12:35:16 +0200

    First commit

---
+++ Miniräknare1/Miniräknare1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miniräknare1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x;
+            //int y;
+
+            Console.WriteLine("skriv in ditt första tal;");
+
+            int x;
+            x = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("skriv in ditt andra tal:");
+
+            int y;
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ange räknesätt:");
+
+            int answer = 0;
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "+":
+                answer = x + y;
+                break;
+
+                case "-":
+                answer = x - y;
+                break;
+
+                case "/":
+                answer = x / y;
+                break;
+
+                case "*":
+                answer = x * y;
+                break;
+
+            }
+            Console.WriteLine(answer);
+            
+            
+                
+            
+          
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Uppgift miniräknare/Uppgift miniräknare/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uppgift_miniräknare
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            string namnF;
+            namnF = label1.Text;
+            
+            string namnE; 
+            namnE= label2.Text;
+            
+            string fullname;
+            fullname = string.Format("{0} {1}",namnF,namnE);
+            
+            listBox1.Items.Add(fullname);
+            
+            textBox1.Text = string.Empty;
+
+            textBox1.Focus();
+        }
+    }
+}
