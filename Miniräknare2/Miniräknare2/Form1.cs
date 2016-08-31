79df97c70b6e6b6dd880d8957e27a05e3bfac5df
sebb <kvakod@gmail.com>
2016-08-31 13:05:17 +0200

    First commit

---
+++ Miniräknare2/Miniräknare2/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Miniräknare2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+            int value1 = int.Parse(textBox1.Text);
+
+            int value2 = int.Parse(textBox2.Text);
+
+            int  result = value1 + value2;
+
+            textBox3.Text = result.ToString(); 
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int value1 = int.Parse(textBox1.Text);
+
+            int value2 = int.Parse(textBox2.Text);
+
+            int result = value1 - value2;
+
+            textBox3.Text = result.ToString();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int value1 = int.Parse(textBox1.Text);
+
+            int value2 = int.Parse(textBox2.Text);
+
+            int result = value1 / value2;
+
+            textBox3.Text = result.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int value1 = int.Parse(textBox1.Text);
+
+            int value2 = int.Parse(textBox2.Text);
+
+            int result = value1 * value2;
+
+            textBox3.Text = result.ToString();
+
+
+        }
+    }
+}
