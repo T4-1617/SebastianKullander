924387867b3e92d532ef37f59718a2a58f0502b2
sebb <kvakod@gmail.com>
2016-09-01 13:08:03 +0200

    First commit

---
+++ Uppgift 0901-1/Uppgift 0901-1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift_0901_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad är ditt hela namn?");
+            string namn;
+            namn = Console.ReadLine();
+            
+
+            Console.WriteLine("Ditt namn " + namn + " innehåller " + namn.Length + " tecken");
+
+            string nynamn = namn.Replace(" ", "");
+            Console.WriteLine("Ditt namn innehåller utan mellanslag " + nynamn.Length + " tecken" );
+
+            string förnamn;
+            förnamn = namn.Split(' ')[0];
+            Console.WriteLine("Ditt förnamn är " + förnamn + " och innehåller " + förnamn.Length + " tecken");
+
+            string efternamn;
+            efternamn = namn.Split(' ')[1];
+            Console.WriteLine("Ditt efternamn är " + efternamn + " och innehåller " + efternamn.Length + " tecken");
+
+            Console.WriteLine(efternamn + " " + förnamn);
+
+
+
+
+
+          }
+
+
+
+
+        }
+    }
+
