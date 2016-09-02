2507d42a7a734947fb51506a8728eab04ce58be5
sebb <kvakod@gmail.com>
2016-09-02 18:37:23 +0200

    tried fixing the errors and added comments

--- Sänkaskepp/Sänkaskepp/Program.cs
+++ Sänkaskepp/Sänkaskepp/Program.cs
@@ -22,7 +22,7 @@ namespace Sänkaskepp
             {
                 for (int x = 0; x < 7; x++)
                 {
-                    gameBoard[x, y] = false;
+                    gameBoard[x, y] = false;//skapar spelplanen
                 }
             }
 
@@ -34,7 +34,7 @@ namespace Sänkaskepp
                 int x = roll.Next(0, 7);
                 int y = roll.Next(0, 5);
                 gameBoard[x, y] = true;
-                Ships--;
+                Ships--;//sätter ut skepp på spelplanen
                 //}
 
                 //for (int y = 0; y < 7; y++)
@@ -52,7 +52,8 @@ namespace Sänkaskepp
                 //        }
                 //        Console.Write(Spot);
                 //    }
-                //    Console.WriteLine();
+                //    Console.WriteLine(); //Fel på denna kod verkar det som
+                //for loop som skriver ut spelplanen i konsolen
 
             }
             Console.WriteLine("Skjut ner skeppen genom att använda rätt kordinater!");
@@ -62,27 +63,53 @@ namespace Sänkaskepp
             int cx = -1;
             int cy = -1;
 
-            while (shipCounter > 0)
+            while (shipCounter > 0)//ska göra så att man fortsätter tills alla skepp är nedskjutna
+                                   //vet ej om det funkar pga att programmet inte funkar
             {
                 Console.WriteLine("X Kordinat:");
                 cx = int.Parse(Console.ReadLine()) - 1;
                 while (cx > 7 || cx < 0)
+                //kontrollerar att man skrivit in korrekt värde
                 {
                     if (cx > 7 || cy < 0)
                     {
                         Console.WriteLine("Kanonen räcker inte dit, det måste vara inom det tillåtna området(1-8)");
                         cx = int.Parse(Console.ReadLine()) - 1;
+                        //om värdet inte är korrekt skickas man tillbaka
                     }
                     Console.WriteLine("Y Kordinat:");
                     cy = int.Parse(Console.ReadLine()) - 1;
                     while (cy > 5 || cy < 0)
                     {
-                        if (cy > 5 || cx < 0)
+                        if (cy > 5 || cy < 0)
                         {
                             Console.WriteLine("Kanonen räcker inte dit, det måste vara inom det tillåtna området(1-6)");
                             cy = int.Parse(Console.ReadLine()) - 1;
+                            {
+                                //    if (gameBoard[cx, cy] == true)
+                                //    {
+                                //        Console.WriteLine("BOOM CRASH POW!");
+
+                                //        gameBoard[cx, cy] = false;
+                                //        shotCounter++;
+                                //        shipCounter--;
+                                //        Console.WriteLine("skepp kvar: " + shipCounter);
+                                //visar om man prickat rätt
+                                //    }
+                                //    else
+                                //    {
+                                //        Console.WriteLine("Illa, försök igen");
+                                //        shotCounter++;
+                                //visar om man missar
+                                //    }
+
+                                //programmet fortsätter att upprepa x kordinaten tills man skrivit in ett felaktigt värde och sedan skriver in
+                                //kordinaten igen.
+                                //även delen av progammet som ska skriva ut banan ger ett error jag inte kunde fixa
+                                //klarade inte av att fixa programmet och jag sitter fast så skrev klart så som jag trodde att det skulle funka
 
 
+                            }
                         }
                     }
                 }
@@ -90,6 +117,7 @@ namespace Sänkaskepp
         }
     }
 }
+
 
 
 
