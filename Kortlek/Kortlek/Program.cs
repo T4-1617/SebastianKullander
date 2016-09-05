0657075ecfce4c31ad345d60c44f1979155cf151
sebb <kvakod@gmail.com>
2016-09-05 15:40:50 +0200

    Initial commit

---
+++ Kortlek/Kortlek/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kortlek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool[,] kortlek = new bool[4, 13];
+
+            for (int x = 0; x < 4; x++)
+            {
+                for (int y = 0; y < 13; y++)
+                {
+                    kortlek[x, y] = false;
+                }
+            }
+            Random roll = new Random();
+            int kort = 52;
+            while (kort > 0)
+            {
+                int y = roll.Next(0, 13);
+                int x = roll.Next(0, 4);
+                if (kortlek[x, y] != true) {
+                    Console.WriteLine(x + " " + y);
+                kortlek[x, y] = true;
+                    kort--;
+                }
+            }
+        }
+    }
+}
