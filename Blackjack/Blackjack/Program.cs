cc2bf26c68d8a1fe139c59885efe9eb950df7577
sebb <kvakod@gmail.com>
2016-09-07 11:33:02 +0200

    added phrases when getting 21 or over

--- Blackjack/Blackjack/Program.cs
+++ Blackjack/Blackjack/Program.cs
@@ -17,99 +17,114 @@ namespace Blackjack
             Random roll = new Random();
             Console.WriteLine("Vill du dra ett kort? J/N");
             char JN = Console.ReadKey().KeyChar;
+            bool Game = true;
 
-
-            while (JN == 'j')
+            while (Game == true)
             {
 
-
-                int x = roll.Next(0, 8);
-                int y = roll.Next(0, 13);
-
-
-                if (CardDeck[x, y] == false)
-                { 
-
-                    switch (x)
+                while (JN == 'j')
                 {
-                    case 0:
-                        Console.Write("ditt kort är hjärter ");
-                        break;
 
-                    case 1:
-                        Console.Write("ditt kort är hjärter ");
-                        break;
 
-                    case 2:
-                        Console.Write("ditt kort är spader ");
-                        break;
+                    int x = roll.Next(0, 8);
+                    int y = roll.Next(0, 13);
 
-                    case 3:
-                        Console.Write("ditt kort är spader ");
-                        break;
 
-                    case 4:
-                        Console.Write("ditt kort är ruter ");
-                        break;
-
-                    case 5:
-                        Console.Write("ditt kort är ruter ");
-                        break;
-
-                    case 6:
-                        Console.Write("ditt kort är klöver ");
-                        break;
-
-                    case 7:
-                        Console.Write("ditt kort är klöver ");
-                        break;
-                 }
-
-                    switch (y)
+                    if (CardDeck[x, y] == false)
                     {
-                        case 0:
-                            Console.WriteLine("ess");
-                            break;
 
-                        case 10:
-                            Console.WriteLine("knekt");
-                            break;
+                        switch (x)
+                        {
+                            case 0:
+                                Console.Write("ditt kort är hjärter ");
+                                break;
 
-                        case 11:
-                            Console.WriteLine("dam");
-                            break;
+                            case 1:
+                                Console.Write("ditt kort är hjärter ");
+                                break;
 
-                        case 12:
-                            Console.WriteLine("kung");
-                            break;
+                            case 2:
+                                Console.Write("ditt kort är spader ");
+                                break;
 
-                        default:
-                            Console.WriteLine(y + 1);
-                            break;
-                        
-                         
+                            case 3:
+                                Console.Write("ditt kort är spader ");
+                                break;
+
+                            case 4:
+                                Console.Write("ditt kort är ruter ");
+                                break;
+
+                            case 5:
+                                Console.Write("ditt kort är ruter ");
+                                break;
+
+                            case 6:
+                                Console.Write("ditt kort är klöver ");
+                                break;
+
+                            case 7:
+                                Console.Write("ditt kort är klöver ");
+                                break;
+                        }
+
+                        switch (y)
+                        {
+                            case 0:
+                                Console.WriteLine("ess");
+                                break;
+
+                            case 10:
+                                Console.WriteLine("knekt");
+                                break;
+
+                            case 11:
+                                Console.WriteLine("dam");
+                                break;
+
+                            case 12:
+                                Console.WriteLine("kung");
+                                break;
+
+                            default:
+                                Console.WriteLine(y + 1);
+                                break;
+
+
+                        }
+                        Points += y + 1;
+                        CardDeck[x, y] = true;
+                        Card--;
+
+                        Console.WriteLine("du har {0} poäng", Points);
+                        Console.WriteLine("\nVill du dra ett till kort? J/N");
+                        JN = Console.ReadKey().KeyChar;
+
                     }
-                    Points += y+1;
-                CardDeck[x, y] = true;
-                Card--;
 
-                Console.WriteLine("du har {0} poäng", Points);
-                Console.WriteLine("\nVill du dra ett till kort? J/N");
-                    JN = Console.ReadKey().KeyChar;
 
                 }
 
-                
+
+
+                while (JN == 'n')
+                {
+                    if (Points == 21)
+                    {
+                        Console.WriteLine("Grattis du vann!");
+                    }
+
+                    else if (Points > 21)
+                    {
+                        Console.WriteLine("Du sprack! bättre lycka nästa gång");
+                    }
+                    Console.WriteLine("du fick {0} poäng", Points);
+                    Console.WriteLine("Vill du köra igen?");
+                    JN = Console.ReadKey().KeyChar;
+                    Points = 0;
+                    
+                }
             }
-
-            
-
-            while (JN == 'n')
-            {
-                Console.WriteLine("du fick {0} poäng", Points);
-                break;
-            }
-
         }
     }
 }
