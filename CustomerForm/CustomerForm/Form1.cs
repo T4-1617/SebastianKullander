42f144e397c688951ea4d770cd3f0b60371d7e71
sebb <kvakod@gmail.com>
2016-09-12 14:30:23 +0200

    Added comments

--- CustomerForm/CustomerForm/Form1.cs
+++ CustomerForm/CustomerForm/Form1.cs
@@ -14,7 +14,7 @@ namespace CustomerForm
     {
         string status;
         int i = 100;
-        
+        //lista som innehåller kunderna
         List<Customer> MyCustomers = new List<Customer>();
 
         public Form1()
@@ -29,17 +29,17 @@ namespace CustomerForm
             c.firstName = textBox1.Text;
             c.lastName = textBox2.Text;
             c.Active = true;
-          
+            //lägger till kunderna
             MyCustomers.Add(c);
-
+            //clearar list boxen
             listBox1.Items.Clear();
-
+            //lägger till kunderna i listboxen
             foreach (Customer item in MyCustomers)
             {
                 listBox1.Items.Add(item.fullName());
             }
 
-            
+            //kollar om statusen ör active eller unactive, kan bara vara active i detta fall
             if (c.Active)
             {
                 status = "Active";
@@ -48,16 +48,18 @@ namespace CustomerForm
             {
                 status = "Unactive";
             }
-
+            //när man trycker på knappen så visar den hur många kunder man har varje gång
             label3.Text = string.Format("du har {0} kunder", MyCustomers.Count);
-
+            //addar 1 på id efter varje kund
             i++;
        
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            //håller koll på vart man klickar i listboxen o hämtar det
             Customer c = MyCustomers[listBox1.SelectedIndex];
+            //skriver ut infon
             MessageBox.Show(string.Format("ID: {0}\nFirst name: {1}\nLast name: {2}\nStatus: {3}",
                 c.id, c.firstName,c.lastName, status ));
         }
