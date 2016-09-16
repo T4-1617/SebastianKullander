667eab50995c55e3b2b28349756d8b35324ed4d2
sebb <kvakod@gmail.com>
2016-09-16 14:26:32 +0200

    Added rent function

--- ACME cars/ACME cars/Form1.Designer.cs
+++ ACME cars/ACME cars/Form1.Designer.cs
@@ -29,15 +29,23 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btnShowCars = new System.Windows.Forms.Button();
+            this.btnAddCar = new System.Windows.Forms.Button();
+            this.btnReturnCar = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lblMake = new System.Windows.Forms.Label();
+            this.lblModel = new System.Windows.Forms.Label();
+            this.lblColor = new System.Windows.Forms.Label();
+            this.btnRent = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -49,89 +57,158 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
             // 
-            // label2
+            // btnShowCars
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(428, 25);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.btnShowCars.Location = new System.Drawing.Point(12, 25);
+            this.btnShowCars.Name = "btnShowCars";
+            this.btnShowCars.Size = new System.Drawing.Size(75, 23);
+            this.btnShowCars.TabIndex = 2;
+            this.btnShowCars.Text = "Show Cars";
+            this.btnShowCars.UseVisualStyleBackColor = true;
+            this.btnShowCars.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button1
+            // btnAddCar
             // 
-            this.button1.Location = new System.Drawing.Point(250, 117);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnAddCar.Location = new System.Drawing.Point(93, 25);
+            this.btnAddCar.Name = "btnAddCar";
+            this.btnAddCar.Size = new System.Drawing.Size(75, 23);
+            this.btnAddCar.TabIndex = 3;
+            this.btnAddCar.Text = "Add Car";
+            this.btnAddCar.UseVisualStyleBackColor = true;
+            this.btnAddCar.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button2
+            // btnReturnCar
             // 
-            this.button2.Location = new System.Drawing.Point(411, 117);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(597, 117);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnReturnCar.Location = new System.Drawing.Point(174, 25);
+            this.btnReturnCar.Name = "btnReturnCar";
+            this.btnReturnCar.Size = new System.Drawing.Size(75, 23);
+            this.btnReturnCar.TabIndex = 4;
+            this.btnReturnCar.Text = "Return Car";
+            this.btnReturnCar.UseVisualStyleBackColor = true;
+            this.btnReturnCar.Click += new System.EventHandler(this.button3_Click);
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.lblMake);
+            this.panel1.Controls.Add(this.lblModel);
+            this.panel1.Controls.Add(this.lblColor);
+            this.panel1.Controls.Add(this.btnRent);
             this.panel1.Controls.Add(this.listBox1);
-            this.panel1.Location = new System.Drawing.Point(167, 420);
+            this.panel1.Location = new System.Drawing.Point(12, 54);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 120);
+            this.panel1.Size = new System.Drawing.Size(253, 120);
             this.panel1.TabIndex = 5;
-            // 
-            // panel2
-            // 
-            this.panel2.Location = new System.Drawing.Point(431, 440);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(200, 100);
-            this.panel2.TabIndex = 0;
-            // 
-            // panel3
-            // 
-            this.panel3.Location = new System.Drawing.Point(700, 440);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(200, 100);
-            this.panel3.TabIndex = 0;
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(3, 22);
+            this.listBox1.Location = new System.Drawing.Point(3, 3);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(194, 95);
             this.listBox1.TabIndex = 6;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.textBox3);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Location = new System.Drawing.Point(275, 299);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(110, 122);
+            this.panel2.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(3, 57);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(3, 31);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(17, 83);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Add";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(334, 52);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 100);
+            this.panel3.TabIndex = 0;
+            // 
+            // lblMake
+            // 
+            this.lblMake.AutoSize = true;
+            this.lblMake.Location = new System.Drawing.Point(203, 12);
+            this.lblMake.Name = "lblMake";
+            this.lblMake.Size = new System.Drawing.Size(0, 13);
+            this.lblMake.TabIndex = 6;
+            // 
+            // lblModel
+            // 
+            this.lblModel.AutoSize = true;
+            this.lblModel.Location = new System.Drawing.Point(203, 35);
+            this.lblModel.Name = "lblModel";
+            this.lblModel.Size = new System.Drawing.Size(0, 13);
+            this.lblModel.TabIndex = 7;
+            // 
+            // lblColor
+            // 
+            this.lblColor.AutoSize = true;
+            this.lblColor.Location = new System.Drawing.Point(203, 59);
+            this.lblColor.Name = "lblColor";
+            this.lblColor.Size = new System.Drawing.Size(0, 13);
+            this.lblColor.TabIndex = 8;
+            // 
+            // btnRent
+            // 
+            this.btnRent.Location = new System.Drawing.Point(200, 75);
+            this.btnRent.Name = "btnRent";
+            this.btnRent.Size = new System.Drawing.Size(38, 23);
+            this.btnRent.TabIndex = 7;
+            this.btnRent.Text = "Rent";
+            this.btnRent.UseVisualStyleBackColor = true;
+            this.btnRent.Click += new System.EventHandler(this.btnRent_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(912, 633);
+            this.ClientSize = new System.Drawing.Size(649, 443);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnReturnCar);
+            this.Controls.Add(this.btnAddCar);
+            this.Controls.Add(this.btnShowCars);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -140,14 +217,21 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnShowCars;
+        private System.Windows.Forms.Button btnAddCar;
+        private System.Windows.Forms.Button btnReturnCar;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblMake;
+        private System.Windows.Forms.Label lblModel;
+        private System.Windows.Forms.Label lblColor;
+        private System.Windows.Forms.Button btnRent;
     }
 }
 
--- ACME cars/ACME cars/Form1.cs
+++ ACME cars/ACME cars/Form1.cs
@@ -20,6 +20,8 @@ namespace ACME_cars
             InitializeComponent();
             Cars = new System.Collections.ArrayList();
 
+            panel1.Visible = false;
+
             Cars.Add(new Car() { Make = "volvo", Model = "v70", Color = "blue", Rented = false });
             Cars.Add(new Car() { Make = "Audi", Model = "A6", Color = "black", Rented = false });
             Cars.Add(new Car() { Make = "Mercedes benz", Model = "AMG", Color = "red", Rented = false });
@@ -46,6 +48,49 @@ namespace ACME_cars
            
         }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
 
+            panel1.Visible = false;
+            panel2.Visible = true;
+            panel3.Visible = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Cars.Add(new Car { Make = textBox1.Text, Model = textBox2.Text, Color = textBox3.Text, Rented = false });
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+            panel2.Visible = false;
+            panel3.Visible = true;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Car x = (Car)listBox1.SelectedItem;
+            panel1.Visible = true;
+            lblMake.Text = x.Make;
+            lblModel.Text = x.Model;
+            lblColor.Text = x.Color;
+        }
+
+        private void btnRent_Click(object sender, EventArgs e)
+        {
+            Car x = (Car)listBox1.SelectedItem;
+            x.Rented = true;
+            listBox1.Items.Clear();
+
+            foreach (Car Item in Cars)
+            {
+                if (!Item.Rented)
+                {
+                    listBox1.Items.Add(Item);
+                    listBox1.DisplayMember = "MakeModel";
+                }
+            }
+        }
     }
 }
