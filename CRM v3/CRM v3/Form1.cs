d1e3dfbe7e7016f7ffc9090ea4e6c30ef3be5f9a
sebb <kvakod@gmail.com>
2016-09-20 12:22:14 +0200

    First commit

---
+++ CRM v3/CRM v3/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRM_v3
+{
+    public partial class Form1 : Form
+    {
+
+        System.Collections.ArrayList People;
+
+        public Form1()
+        {
+            InitializeComponent();
+            People = new System.Collections.ArrayList();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
