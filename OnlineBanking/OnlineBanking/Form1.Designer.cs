3f2f390900d8b710995ed59b5b97cf3cd7eea77f
sebb <kvakod@gmail.com>
2016-09-22 12:36:57 +0200

    commit for pc change

--- OnlineBanking/OnlineBanking/Form1.Designer.cs
+++ OnlineBanking/OnlineBanking/Form1.Designer.cs
@@ -39,12 +39,18 @@
             this.listBox2 = new System.Windows.Forms.ListBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(413, 24);
+            this.label1.Location = new System.Drawing.Point(208, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(88, 13);
             this.label1.TabIndex = 0;
@@ -53,14 +59,14 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(398, 52);
+            this.comboBox1.Location = new System.Drawing.Point(193, 25);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 1;
             // 
             // btnWithdrawMoney
             // 
-            this.btnWithdrawMoney.Location = new System.Drawing.Point(260, 249);
+            this.btnWithdrawMoney.Location = new System.Drawing.Point(12, 282);
             this.btnWithdrawMoney.Name = "btnWithdrawMoney";
             this.btnWithdrawMoney.Size = new System.Drawing.Size(89, 69);
             this.btnWithdrawMoney.TabIndex = 2;
@@ -69,7 +75,7 @@
             // 
             // AccountBalance
             // 
-            this.AccountBalance.Location = new System.Drawing.Point(260, 324);
+            this.AccountBalance.Location = new System.Drawing.Point(12, 357);
             this.AccountBalance.Name = "AccountBalance";
             this.AccountBalance.Size = new System.Drawing.Size(89, 69);
             this.AccountBalance.TabIndex = 3;
@@ -78,7 +84,7 @@
             // 
             // btnOpenAccount
             // 
-            this.btnOpenAccount.Location = new System.Drawing.Point(260, 99);
+            this.btnOpenAccount.Location = new System.Drawing.Point(12, 132);
             this.btnOpenAccount.Name = "btnOpenAccount";
             this.btnOpenAccount.Size = new System.Drawing.Size(89, 69);
             this.btnOpenAccount.TabIndex = 4;
@@ -87,7 +93,7 @@
             // 
             // btnDepositMoney
             // 
-            this.btnDepositMoney.Location = new System.Drawing.Point(260, 174);
+            this.btnDepositMoney.Location = new System.Drawing.Point(12, 207);
             this.btnDepositMoney.Name = "btnDepositMoney";
             this.btnDepositMoney.Size = new System.Drawing.Size(89, 69);
             this.btnDepositMoney.TabIndex = 5;
@@ -96,7 +102,7 @@
             // 
             // Engagemang
             // 
-            this.Engagemang.Location = new System.Drawing.Point(260, 409);
+            this.Engagemang.Location = new System.Drawing.Point(12, 449);
             this.Engagemang.Name = "Engagemang";
             this.Engagemang.Size = new System.Drawing.Size(89, 69);
             this.Engagemang.TabIndex = 6;
@@ -106,9 +112,9 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(812, 148);
+            this.listBox1.Location = new System.Drawing.Point(812, 81);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.Size = new System.Drawing.Size(120, 160);
             this.listBox1.TabIndex = 7;
             // 
             // listBox2
@@ -116,13 +122,13 @@
             this.listBox2.FormattingEnabled = true;
             this.listBox2.Location = new System.Drawing.Point(812, 298);
             this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(120, 95);
+            this.listBox2.Size = new System.Drawing.Size(120, 160);
             this.listBox2.TabIndex = 8;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(846, 115);
+            this.label2.Location = new System.Drawing.Point(846, 48);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(52, 13);
             this.label2.TabIndex = 9;
@@ -137,11 +143,61 @@
             this.label3.TabIndex = 10;
             this.label3.Text = "Välj konto";
             // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(107, 57);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(325, 252);
+            this.panel1.TabIndex = 11;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(107, 315);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(325, 252);
+            this.panel2.TabIndex = 12;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(438, 57);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(325, 252);
+            this.panel3.TabIndex = 13;
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(438, 315);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(325, 252);
+            this.panel4.TabIndex = 14;
+            // 
+            // panel5
+            // 
+            this.panel5.Location = new System.Drawing.Point(107, 573);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(325, 252);
+            this.panel5.TabIndex = 15;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 69);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Ny kund";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(993, 661);
+            this.ClientSize = new System.Drawing.Size(993, 920);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.listBox2);
@@ -173,6 +229,12 @@
         private System.Windows.Forms.ListBox listBox2;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button button1;
     }
 }
 
