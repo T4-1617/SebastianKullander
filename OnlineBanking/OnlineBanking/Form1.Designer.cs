a1323b4b9fc7304a94a57ada7e3e7eaa050df500
sebb <kvakod@gmail.com>
2016-09-22 11:52:13 +0200

    First commit

--- OnlineBanking/OnlineBanking/Form1.Designer.cs
+++ OnlineBanking/OnlineBanking/Form1.Designer.cs
@@ -28,12 +28,151 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnWithdrawMoney = new System.Windows.Forms.Button();
+            this.AccountBalance = new System.Windows.Forms.Button();
+            this.btnOpenAccount = new System.Windows.Forms.Button();
+            this.btnDepositMoney = new System.Windows.Forms.Button();
+            this.Engagemang = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(413, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Välj användarroll:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(398, 52);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // btnWithdrawMoney
+            // 
+            this.btnWithdrawMoney.Location = new System.Drawing.Point(260, 249);
+            this.btnWithdrawMoney.Name = "btnWithdrawMoney";
+            this.btnWithdrawMoney.Size = new System.Drawing.Size(89, 69);
+            this.btnWithdrawMoney.TabIndex = 2;
+            this.btnWithdrawMoney.Text = "Ta ut $";
+            this.btnWithdrawMoney.UseVisualStyleBackColor = true;
+            // 
+            // AccountBalance
+            // 
+            this.AccountBalance.Location = new System.Drawing.Point(260, 324);
+            this.AccountBalance.Name = "AccountBalance";
+            this.AccountBalance.Size = new System.Drawing.Size(89, 69);
+            this.AccountBalance.TabIndex = 3;
+            this.AccountBalance.Text = "Saldo";
+            this.AccountBalance.UseVisualStyleBackColor = true;
+            // 
+            // btnOpenAccount
+            // 
+            this.btnOpenAccount.Location = new System.Drawing.Point(260, 99);
+            this.btnOpenAccount.Name = "btnOpenAccount";
+            this.btnOpenAccount.Size = new System.Drawing.Size(89, 69);
+            this.btnOpenAccount.TabIndex = 4;
+            this.btnOpenAccount.Text = "Öppna konto";
+            this.btnOpenAccount.UseVisualStyleBackColor = true;
+            // 
+            // btnDepositMoney
+            // 
+            this.btnDepositMoney.Location = new System.Drawing.Point(260, 174);
+            this.btnDepositMoney.Name = "btnDepositMoney";
+            this.btnDepositMoney.Size = new System.Drawing.Size(89, 69);
+            this.btnDepositMoney.TabIndex = 5;
+            this.btnDepositMoney.Text = "Sätt in $";
+            this.btnDepositMoney.UseVisualStyleBackColor = true;
+            // 
+            // Engagemang
+            // 
+            this.Engagemang.Location = new System.Drawing.Point(260, 409);
+            this.Engagemang.Name = "Engagemang";
+            this.Engagemang.Size = new System.Drawing.Size(89, 69);
+            this.Engagemang.TabIndex = 6;
+            this.Engagemang.Text = "Engagemang";
+            this.Engagemang.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(812, 148);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 7;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(812, 298);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 95);
+            this.listBox2.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(846, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Välj Kund";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(855, 277);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Välj konto";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(993, 661);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Engagemang);
+            this.Controls.Add(this.btnDepositMoney);
+            this.Controls.Add(this.btnOpenAccount);
+            this.Controls.Add(this.AccountBalance);
+            this.Controls.Add(this.btnWithdrawMoney);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnWithdrawMoney;
+        private System.Windows.Forms.Button AccountBalance;
+        private System.Windows.Forms.Button btnOpenAccount;
+        private System.Windows.Forms.Button btnDepositMoney;
+        private System.Windows.Forms.Button Engagemang;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
