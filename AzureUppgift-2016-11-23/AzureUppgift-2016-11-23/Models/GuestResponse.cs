1f3adb7dd05bf641bc1df3f68c593bdbe0fae77a
sebb <kvakod@gmail.com>
2016-11-23 14:58:28 +0100

    Added Class GuestResponse.

---
+++ AzureUppgift-2016-11-23/AzureUppgift-2016-11-23/Models/GuestResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AzureUppgift_2016_11_23.Models
+{
+    public class GuestResponse
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage ="Ange din adress!")]
+        public string Adress { get; set; }
+
+        public string Telephone { get; set; }
+    }
+}
