ecbb7b941ed13c1c1c9fc5e229941137f5bb10c5
sebb <kvakod@gmail.com>
2016-11-24 16:34:27 +0100

    Able to store information in database

--- AzureUppgift-2016-11-23/AzureUppgift-2016-11-23/Controllers/HomeController.cs
+++ AzureUppgift-2016-11-23/AzureUppgift-2016-11-23/Controllers/HomeController.cs
@@ -4,11 +4,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
 
 namespace AzureUppgift_2016_11_23.Controllers
 {
     public class HomeController : Controller
     {
+        private CloudStorageAccount storageAccount;
+        private CloudTableClient tableClient;
+        private CloudTable table;
+
+        public HomeController()
+        {
+            storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=http;AccountName=sebbstorage;AccountKey=X6psta3fD7F93R/LMnYClB5E3N8gavdOByzdlrGqpg/xlQjFfQCoXqrJ9+0tuoHkbk24eqIm8ZshHzNuDUiFIQ==;");
+            tableClient = storageAccount.CreateCloudTableClient();
+            table = tableClient.GetTableReference("LocalTable");
+            table.CreateIfNotExists();
+        }
+
         // GET: Home
         public ActionResult Index()
         {
@@ -26,9 +40,13 @@ namespace AzureUppgift_2016_11_23.Controllers
         {
             if (ModelState.IsValid == true)
             {
+                GuestResponse person = new GuestResponse(Guid.NewGuid().ToString()) { Name = g.Name, Telephone = g.Telephone, Adress = g.Adress};
+
+                TableOperation insertOperation = TableOperation.Insert(person);
+
+                table.Execute(insertOperation);
                 return View("Thanks", g);
             }
-
             return View();
         }
     }
--- AzureUppgift-2016-11-23/AzureUppgift-2016-11-23/Models/GuestResponse.cs
+++ AzureUppgift-2016-11-23/AzureUppgift-2016-11-23/Models/GuestResponse.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,8 +7,21 @@ using System.Web;
 
 namespace AzureUppgift_2016_11_23.Models
 {
-    public class GuestResponse
+    public class GuestResponse : TableEntity
     {
+
+        public GuestResponse()
+        {
+
+        }
+    
+        public GuestResponse(string rowKey)
+        {
+            this.PartitionKey = "KoraBil";
+            this.RowKey = rowKey;
+        }
+
+
         [Required(ErrorMessage ="Ange ditt namn!")]
         public string Name { get; set; }
 
