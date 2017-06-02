2405ea156da82dbd8a92822ea390c4c35221f236
SebastianKullander <Kvakod@gmail.com>
2017-06-02 03:07:23 +0200

    Added some different elements and picture and played with the values of the "scroll speed"

---
+++ NewPortfolio/NewPortfolio/Scripts/Parallax.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+
+    $(window).bind('scroll', function (e) {
+        parallaxScroll();
+    });
+
+    function parallaxScroll() {
+        var scrolled = $(window).scrollTop();
+        $('#parallax1').css('top', (0 - (scrolled * .25)) + 'px');
+        $('#parallax2').css('top', (0 - (scrolled * 0.5)) + 'px');
+        $('#parallax3').css('top', (0 - (scrolled * 0.75)) + 'px');
+        $('#parallax4').css('top', (0 - (scrolled * 2)) + 'px');
+        $('#parallax5').css('top', (0 - (scrolled * 4)) + 'px');
+    }
+}
+)
