0498be65d8a13ce5ba9b70e674f37f2c9f4f24bf
SebastianKullander <Kvakod@gmail.com>
2017-06-02 23:25:24 +0200

    Fast commit because i need to go

--- NewPortfolio/NewPortfolio/Scripts/Parallax.js
+++ NewPortfolio/NewPortfolio/Scripts/Parallax.js
@@ -6,11 +6,10 @@
 
     function parallaxScroll() {
         var scrolled = $(window).scrollTop();
-        $('#parallax1').css('top', (0 - (scrolled * .25)) + 'px');
-        $('#parallax2').css('top', (0 - (scrolled * 0.5)) + 'px');
-        $('#parallax3').css('top', (0 - (scrolled * 0.75)) + 'px');
-        $('#parallax4').css('top', (0 - (scrolled * 2)) + 'px');
-        $('#parallax5').css('top', (0 - (scrolled * 4)) + 'px');
+        $('#parallax-bg1').css('top', (0 - (scrolled * .25)) + 'px');
+        $('#parallax-bg2').css('top', (0 - (scrolled * 0.5)) + 'px');
+        $('#parallax-bg3').css('top', (0 - (scrolled * 0.75)) + 'px');
+        $('#parallax-bg4').css('top', (0 - (scrolled * 2)) + 'px');
     }
 }
 )
